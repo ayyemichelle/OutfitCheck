62fe251066ca3c666c44c221f4b2d194f7203c54
karla <kabadtor@uci.edu>
2020-03-11 12:59:54 -0700

    Creating Xcode project in a separate branch

---
+++ OutfitCheck/OutfitCheck/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OutfitCheck
+//
+//  Created by Karla on 3/11/20.
+//  Copyright © 2020 Karla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
