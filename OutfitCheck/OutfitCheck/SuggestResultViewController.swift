dce1c307bf31e1b834b211917913f3cd1d197840
Lynette Nguyen <lynettenguyen0909@gmail.com>
2020-03-22 20:11:04 -0700

    Added outlets for Search Results VC

--- OutfitCheck/OutfitCheck/SuggestResultViewController.swift
+++ OutfitCheck/OutfitCheck/SuggestResultViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class SuggestResultViewController: UIViewController {
 
+    
+    @IBOutlet weak var outerwearImage: UIImageView!
+    @IBOutlet weak var outwearLabel: UILabel!
+    @IBOutlet weak var topImage: UIImageView!
+    @IBOutlet weak var topLabel: UILabel!
+    
+    @IBOutlet weak var bottomImage: UIImageView!
+    @IBOutlet weak var bottomLabel: UILabel!
+    @IBOutlet weak var shoeImage: UIImageView!
+    @IBOutlet weak var shoeLabel: UILabel!
+    
+    
+    @IBOutlet weak var startImage: UIImageView!
+    
+    @IBOutlet weak var startTime: UILabel!
+    @IBOutlet weak var startTemp: UILabel!
+    @IBOutlet weak var startConditions: UILabel!
+    
+    @IBOutlet weak var endImage: UIImageView!
+    @IBOutlet weak var endTime: UILabel!
+    @IBOutlet weak var endTemp: UILabel!
+    @IBOutlet weak var endConditions: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
